8b6c249085a6dd9a2ad2c15caf027a7e46973ab7
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 23:21:24 +0200

    Update settings.js for Merufxe-harcker

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;KZMi3RiR#WA5ovliFhDJ6SsOOX1sL8s19M5Mo-ahqDszRHb-kewA",
+  OWNER_NUMBER: "+2349116221205", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
