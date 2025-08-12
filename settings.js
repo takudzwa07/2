1410ab34b480b55547d92d2dfff2ce9dee613dc7
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 08:03:08 +0200

    Update settings.js for Aziz

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;WJUmUJpb#PbGW1x2OVYj-7zdytQuVydT9Q2LCoWDRn5Eubd0vzxo",
+  OWNER_NUMBER: "93780008566", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
