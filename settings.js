24e45419a114607d94233868c119444ca3421275
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-12 08:33:05 +0200

    Update settings.js for bartho-rohit

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;3RtWiDiR#E86Sc_Xi9zmtyguHBs8bXt-I9N-wpnBpKoEXbUpdzHc",
+  OWNER_NUMBER: "+919382993742", 
+  PREFIX: "S"
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
