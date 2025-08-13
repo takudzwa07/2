f56a215783ab7437572dc08fa89c71729069c0ff
takudzwa07 <mrfrankofc17@gmail.com>
2025-08-13 16:45:16 +0200

    Update settings.js for Alexis

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO~745d55f7592475b1390976c9075a525f55fbd952", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
+  SESSION_ID: "SUBZERO-MD;;;PUlHSJKQ#0SN65nO5pacWNXn5aviip18R6JxZAxBPwTaVzz8yITI",
+  OWNER_NUMBER: "+2348038712488", 
+  PREFIX: "."
 };
-
-// CREATED BY DARRELL MUCHERI ( MR FRANK OFC 🇿🇼 )
